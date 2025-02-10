234289de5795d28e7644d60177fdfcedfaf98f82
Ruairi Delahunt <ruairidelahunt@gmail.com>
2025-02-10 15:09:51 -0800

    0.9 test

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -5,10 +5,32 @@ public class LevelChanger : MonoBehaviour
 {
     [Header("Scene Settings")]
     public string sceneToLoad; // Name of the scene to load
+    public bool requireKeyPress = false; // Toggle for requiring "E" key press
+    private bool playerInTrigger = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // Ensure only the player triggers the level change
+        {
+            playerInTrigger = true;
+            if (!requireKeyPress)
+            {
+                LoadNextLevel();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (requireKeyPress && playerInTrigger && Input.GetKeyDown(KeyCode.E))
         {
             LoadNextLevel();
         }
